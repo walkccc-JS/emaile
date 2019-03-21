1122a9151dff8e77a626e0ee3fabdd5c1a270998
walkccc <walkccray@gmail.com>
2019-03-22 01:02:06 +0800

    Version 0 released

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 const Dashboard = () => {
   return (
     <section className="section">
-      <div className="container" style={{ maxWidth: 1024, textAlign: 'center' }}>
-        <div className="notification is-link">
+      <div className="container" style={{ maxWidth: 1024 }}>
+        <SurveyList />
+        <div className="notification is-link" style={{ textAlign: 'center' }}>
           <h1 style={{ fontSize: '3rem' }}>
             <strong>Dashboard</strong>
           </h1>
---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys() {
+    return this.props.surveys.reverse().map(survey => {
+      return (
+        <div className="card" key={survey._id} style={{ marginBottom: '1rem' }}>
+          <div className="card-content">
+            <div className="media">
+              <div className="media-content">
+                <p className="title is-4">{survey.title}</p>
+                <p className="subtitle is-6">{survey.subject}</p>
+              </div>
+            </div>
+
+            <div className="content">
+              {survey.body}
+              <br />
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </div>
+          </div>
+          <footer className="card-footer">
+            <Link to="#" className="card-footer-item has-text-info">
+              <i className="fas fa-thumbs-up" style={{ marginRight: '1rem' }} />
+              {survey.yes}
+            </Link>
+            <Link to="#" className="card-footer-item has-text-danger">
+              <i className="fas fa-thumbs-down" style={{ marginRight: '1rem' }} />
+              {survey.no}
+            </Link>
+          </footer>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div>{this.renderSurveys()}</div>;
+  }
+}
+
+function mapStateToProps({ surveys }) {
+  return { surveys };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchSurveys }
+)(SurveyList);
